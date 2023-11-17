87ef6ff5b87c30135834aeed28d1a24a58e687d4
MizukiNonoyama <germaniumdiode1139@gmail.com>
2023-11-17 15:52:14 +0900

    plotのguiをzoom out

--- parameter_calculator/src/parameter_calculator/gui/GuiConfig.java
+++ parameter_calculator/src/parameter_calculator/gui/GuiConfig.java
@@ -5,11 +5,11 @@ public class GuiConfig {
 	
 	public static double scale_x = 2.0;
 	public static double scale_x_small = 0.0;
-	public static double scale_y = 0.5;
-	public static double scale_y_small = 0.05;
+	public static double scale_y = 2.0;
+	public static double scale_y_small = 0.2;
 	
 	public static double min_plot_x = -5.0;
 	public static double max_plot_x = 10.0;
 	public static double min_plot_y = -2.0;
-	public static double max_plot_y = 2.0;
+	public static double max_plot_y = 10.0;
 }
