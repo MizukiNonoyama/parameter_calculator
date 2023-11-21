0df0c72165e11b72a885c9444c2708cb39ac9217
MizukiNonoyama <germaniumdiode1139@gmail.com>
2023-11-21 18:58:07 +0900

    Update gui parameter

--- parameter_calculator/src/parameter_calculator/gui/GuiConfig.java
+++ parameter_calculator/src/parameter_calculator/gui/GuiConfig.java
@@ -3,13 +3,13 @@ package parameter_calculator.gui;
 public class GuiConfig {
 	public static double resolution = 0.01;
 	
-	public static double scale_x = 2.0;
-	public static double scale_x_small = 0.0;
-	public static double scale_y = 2.0;
-	public static double scale_y_small = 0.2;
-	
 	public static double min_plot_x = -5.0;
 	public static double max_plot_x = 10.0;
-	public static double min_plot_y = -2.0;
-	public static double max_plot_y = 10.0;
+	public static double max_plot_y = 30.0;
+	public static double min_plot_y = -max_plot_y / 5.0;
+	
+	public static double scale_x = 2.0;
+	public static double scale_x_small = 0.0;
+	public static double scale_y = max_plot_y / 5.0;
+	public static double scale_y_small = scale_y / 10.0;
 }
